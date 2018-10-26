e44631e55098996d5cd5692569c32f551fa668b9
mingchipc <priscilla199685@gmail.com>
2018-10-26 12:19:03 -0600

    LinkedList

---
+++ Lab#2_1/src/test/ListElement.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author tsai203m
+ *
+ */
+public class ListElement {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
